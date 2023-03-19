6af96b2f9b86b5c512a5edf7918f8c35e6a43576
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-19 19:58:31 +0800

    Constant in C sharp

--- Learn-C-/Program.cs
+++ Learn-C-/Program.cs
@@ -1,28 +1,7 @@
-﻿int x; // Declaration
-x = 100; // Initialization
-int y = 200; // Declaration + Initialization
+﻿const double pi = 3.14; // cannot be changed during program running
 
-Console.WriteLine("Value y is " + y);
-Console.WriteLine("Data type y is " + y.GetType() + "\n");
+// pi = 100.1; << will get an error
 
-int age = 25; // store whole integer
-Console.WriteLine(age);
-Console.WriteLine("Data Type age is " + age.GetType() + "\n");
-
-double height = 172.5; // store decimal value
-Console.WriteLine(height);
-Console.WriteLine("Data Type height is " + height.GetType() + "\n");
-
-bool alive = true; // store boolean value
-Console.WriteLine(alive);
-Console.WriteLine("Data type alive is " + alive.GetType() + "\n");
-
-char code = 'Z'; // store only a character  // mus uer single ' ' Quotes
-Console.WriteLine(code);
-Console.WriteLine("Data type code is " + code.GetType() + "\n");
-
-string username = "Muhammad_Khuzain";
-Console.WriteLine(username);
-Console.WriteLine("Data type username is " + username.GetType() + "\n");
-
+Console.WriteLine(pi);
+Console.WriteLine(pi.GetTypeCode());
 
