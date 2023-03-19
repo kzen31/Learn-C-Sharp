1d09c99753ebf5abeb3e99b2d3408650d66c0c89
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-19 19:51:43 +0800

    Data type in C sharp

--- Learn-C-/Program.cs
+++ Learn-C-/Program.cs
@@ -1,17 +1,28 @@
-﻿// Default console write line (using shortcut 'cw')
-Console.WriteLine("Hello Bro");
+﻿int x; // Declaration
+x = 100; // Initialization
+int y = 200; // Declaration + Initialization
 
-Console.WriteLine("Hello Mr. Muhammad Khuzain");
+Console.WriteLine("Value y is " + y);
+Console.WriteLine("Data type y is " + y.GetType() + "\n");
 
-// Using escape sequence
-Console.WriteLine("\nNama\nSaya\nMuhammad\nKhuzain\n");
+int age = 25; // store whole integer
+Console.WriteLine(age);
+Console.WriteLine("Data Type age is " + age.GetType() + "\n");
 
-// Using inline "Console.Write"
-Console.Write("This is ");
-Console.WriteLine("Inline");
+double height = 172.5; // store decimal value
+Console.WriteLine(height);
+Console.WriteLine("Data Type height is " + height.GetType() + "\n");
 
-// Using tab
-Console.WriteLine("\t this is tab");
+bool alive = true; // store boolean value
+Console.WriteLine(alive);
+Console.WriteLine("Data type alive is " + alive.GetType() + "\n");
 
-// Using single and double qoute
-Console.WriteLine("This is \"brother\" and \'sister\' \\");
+char code = 'Z'; // store only a character  // mus uer single ' ' Quotes
+Console.WriteLine(code);
+Console.WriteLine("Data type code is " + code.GetType() + "\n");
+
+string username = "Muhammad_Khuzain";
+Console.WriteLine(username);
+Console.WriteLine("Data type username is " + username.GetType() + "\n");
+
+
