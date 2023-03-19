a1defa27a3de634a650d34461bd9ede3c50ff3af
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-19 11:34:31 +0800

    set output in c sharp

--- Learn-C-/Program.cs
+++ Learn-C-/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Default console write line (using shortcut 'cw')
+Console.WriteLine("Hello Bro");
+
+Console.WriteLine("Hello Mr. Muhammad Khuzain");
+
+// Using escape sequence
+Console.WriteLine("\nNama\nSaya\nMuhammad\nKhuzain\n");
+
+// Using inline "Console.Write"
+Console.Write("This is ");
+Console.WriteLine("Inline");
+
+// Using tab
+Console.WriteLine("\t this is tab");
+
+// Using single and double qoute
+Console.WriteLine("This is \"brother\" and \'sister\' \\");
