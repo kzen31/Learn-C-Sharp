03d5051fedb9607da02bf3e1d545c33e3c0d7c27
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-22 10:23:14 +0800

    Pseudo random number in C#

--- Learn-C-/Program.cs
+++ Learn-C-/Program.cs
@@ -1,17 +1,6 @@
-﻿// Math Class
+﻿// Pseudo random number
 
-Console.WriteLine("Math.Pow 9 by 2 is " + Math.Pow(9, 2));
+Random random = new Random();
 
-Console.WriteLine("Math.Sqrt 9 is " + Math.Sqrt(9));
-
-Console.WriteLine("Math.Max(9, 2) is " + Math.Max(9, 2));
-
-Console.WriteLine("Math.Min(9,2) is " + Math.Min(9, 2));
-
-Console.WriteLine("Math.Abs(-32) is " + Math.Abs(-32));
-
-Console.WriteLine("Pembulatan Math.Round(3.54) is " + Math.Round(3.54));
-
-Console.WriteLine("Pembulatan ke bawah Math.Floor(3.54) is " + Math.Floor(3.54));
-
-Console.WriteLine("Pembulatan ke atas Math.Ceiling(3.54) is " + Math.Ceiling(3.54));
+Console.WriteLine(random.Next(1, 7)); // will generate random number 1, 2, 3, 4, 5, 6
+Console.WriteLine(random.NextDouble()); // will generate random decimal double number between 0 and 1
