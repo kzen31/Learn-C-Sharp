0e0e6a301b5ac217d6745553188436975ab2297c
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-22 17:31:23 +0800

    string in c#

--- Learn-C-/Program.cs
+++ Learn-C-/Program.cs
@@ -1,11 +1,33 @@
-﻿// Application to count sisi miring segitiga siku2
+﻿// string in c#
 
-Console.WriteLine("Masukkan nilai sisi tegak: ");
-double sisiTegak = Convert.ToDouble(Console.ReadLine());
+string name = "Muhammad Khuzain";
+String fullName = "Muhammad Khuzain";
 
-Console.WriteLine("Masukkan nilai sisi mendatar: ");
-double sisiMendatar = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Name = " + name);
 
-double sisiMiring = Math.Sqrt(sisiTegak * sisiTegak + sisiMendatar * sisiMendatar);
-Console.WriteLine("Sisi miring segitiga yaitu: ");
-Console.WriteLine(sisiMiring);
+// Concat Method
+Console.WriteLine("Concat = " + String.Concat(name, fullName));
+
+// ToUpper Method
+Console.WriteLine("string.ToUpper() = " + name.ToUpper());
+
+// ToLower Method
+Console.WriteLine("string.ToLower() = " + name.ToLower());
+
+// Replace Method
+Console.WriteLine("string.Replace(' ', '-') = " + fullName.Replace(" ", "_"));
+
+// Insert Method
+Console.WriteLine("string.Insert(0, 'Mr. ') = "  + fullName.Insert(0, "Mr. "));
+
+// Length Method
+Console.WriteLine("string.Length = " + fullName.Length);
+
+// Substring Method
+Console.WriteLine("string.Substring(start, end) ex. (0,5) = " + name.Substring(0, 5));
+
+/* Difference between String and string
+ * overall string and String is same,
+ * but string often use for declaring a variable, and
+ * String is a Class to use full veatured
+ */
