0a9c7313233d40dee847a9cea74d10ae072c1fbd
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-22 08:42:25 +0800

    operators

--- Learn-C-/Program.cs
+++ Learn-C-/Program.cs
@@ -1,14 +1,94 @@
-﻿Console.WriteLine("Input your name here ");
-string name = Console.ReadLine();
+﻿// Arithmetic
 
-Console.WriteLine("Your name is " + name);
+int x = 500;
 
-Console.WriteLine("Input your age here");
-int age = Convert.ToInt32(Console.ReadLine());
+int y = 100;
 
-Console.WriteLine("Your age is " + age);
+int zAdd = x + y;
+Console.WriteLine("Addition 500 and 100 is " + zAdd);
 
-/* Catatan:
- * input yang dimasukkan melalui console selalu string,
- * oleh sebab itu perlu dikonversi dan perlu handle exception jika format input tidak sesuai dengan yang ditentukan
-*/
+int zSub = x - y;
+Console.WriteLine("Subtraction 500 and 100 is " + zSub);
+
+int zMultip = x * y;
+Console.WriteLine("Multiplication 500 and 100 is " + zMultip);
+
+int zDiv = x / y;
+Console.WriteLine("Division 500 and 100 is " + zDiv);
+
+int zMod = x % y;
+Console.WriteLine("Modulus 500 and 100 is " + zMod);
+
+Console.WriteLine("Result x++ is " + x++);
+
+Console.WriteLine("Result x-- is " + x--);
+
+// Assignment
+// using symbol equal '=';
+
+Console.WriteLine("\nx = 500, result " + x);
+
+x += 50;
+Console.WriteLine("x += 50, result " + x);
+
+x = 500;
+x -= 50;
+Console.WriteLine("x -= 50, result " + x);
+
+x = 500;
+x /= 50;
+Console.WriteLine("x /= 50, result " + x);
+
+x = 500;
+x *= 50;
+Console.WriteLine("x *= 50, result " + x);
+
+x = 500;
+x %= 50;
+Console.WriteLine("x %= 50, result " + x);
+
+x = 5;
+x &= 7;
+Console.WriteLine("x = 5; x &= 7 is equal 101 AND 111 = 101, result " + x);
+
+x = 5;
+x |= 7;
+Console.WriteLine("x = 5; x |= 7 is equal 101 OR 111 = 111, result " + x);
+
+x = 5;
+x ^= 3;
+Console.WriteLine("x ^= 3 is equal to 101 NAND 11 = 110, result " + x);
+
+x = 5;
+x >>= 1;
+Console.WriteLine("x >>= 1 is equal to remove 1 digit in the end of biner 101 = 10, result " + x);
+
+x = 5;
+x <<= 3;
+Console.WriteLine("x <<= 3 is equal to add 3 digit of 0 zero in the end of biner 101 = 101000, or 5 * (2^3) result " + x);
+
+// Comparison
+// return boolean (true or false) comparing to variable
+
+x = 5;
+y = 7;
+
+Console.WriteLine("\nx = 5, y = 7;");
+Console.WriteLine("x == y, result " + (x == y));
+
+Console.WriteLine("x >= y, result " + (x >= y));
+
+Console.WriteLine("x <= y, result " + (x <= y));
+
+Console.WriteLine("x != y, result " + (x != y));
+
+Console.WriteLine("x > y, result " + (x > y));
+
+Console.WriteLine("x < y, result " + (x < y));
+
+
+
+// Logical
+// && logical AND
+// || logical OR
+// ! logical NOT
