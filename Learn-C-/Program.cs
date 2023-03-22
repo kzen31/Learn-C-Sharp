1b76a62420f52f7387da5242491a327437b19cd8
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-22 17:46:03 +0800

    if statement in c#

--- Learn-C-/Program.cs
+++ Learn-C-/Program.cs
@@ -1,33 +1,29 @@
-﻿// string in c#
+﻿// If StateMent
 
-string name = "Muhammad Khuzain";
-String fullName = "Muhammad Khuzain";
+Console.WriteLine("Enter your age : ");
 
-Console.WriteLine("Name = " + name);
+string ageString = Console.ReadLine();
 
-// Concat Method
-Console.WriteLine("Concat = " + String.Concat(name, fullName));
+if (ageString !="")
+    {
+        int age = Convert.ToInt32(ageString);
 
-// ToUpper Method
-Console.WriteLine("string.ToUpper() = " + name.ToUpper());
+        if (age > 100)
+        {
+            Console.WriteLine("You are too old");
+        }
+        else if (age >= 17)
+        {
+            Console.WriteLine("You can enter the party");
+        }
+        else if (age < 0)
+        {
+            Console.WriteLine("You have not born yet");
+        }
+        else
+        {
+            Console.WriteLine("You can not enter the party");
+        }
 
-// ToLower Method
-Console.WriteLine("string.ToLower() = " + name.ToLower());
+    }
 
-// Replace Method
-Console.WriteLine("string.Replace(' ', '-') = " + fullName.Replace(" ", "_"));
-
-// Insert Method
-Console.WriteLine("string.Insert(0, 'Mr. ') = "  + fullName.Insert(0, "Mr. "));
-
-// Length Method
-Console.WriteLine("string.Length = " + fullName.Length);
-
-// Substring Method
-Console.WriteLine("string.Substring(start, end) ex. (0,5) = " + name.Substring(0, 5));
-
-/* Difference between String and string
- * overall string and String is same,
- * but string often use for declaring a variable, and
- * String is a Class to use full veatured
- */
